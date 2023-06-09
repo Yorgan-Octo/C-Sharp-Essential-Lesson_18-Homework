e6495ab9891e21b7372f23b79c5fe00e9bf1ab06
Artem Pr <yorgan.info@gmail.com>
2023-06-09 15:29:13 +0300

    Завершение задания 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserDictionary; // мое завдання 3 из 14 урока подключил как библитеку класов
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+
+            myDictionary.Add("Va1", 78);
+            myDictionary.Add("Va2", 56);
+            myDictionary.Add("Va3", 567);
+            myDictionary.Add("Va4", 543);
+            myDictionary.Add("Va5", 234);
+
+            foreach (var item in myDictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+
+            if (myDictionary.ContainsKey("Va4"))
+            {
+                Console.WriteLine("Такий ключ Є тому занчення " + myDictionary["Va4"]);
+            }
+            else
+            {
+                Console.WriteLine("Такого ключа немае");
+            }
+
+            if (myDictionary.ContainsKey("Va99"))
+            {
+                Console.WriteLine("Такий ключ Є тому занчення " + myDictionary["Va499"]);
+            }
+            else
+            {
+                Console.WriteLine("Такого ключа немае");
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
